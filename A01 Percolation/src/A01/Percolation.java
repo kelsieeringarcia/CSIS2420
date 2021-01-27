79155eb515eca0c17a99123d6fd272d6c7f773b6
kelsieeringarcia <36800046+kelsieeringarcia@users.noreply.github.com>
2021-01-27 15:41:23 -0700

    headers for project

---
+++ A01 Percolation/src/A01/Percolation.java
@@ -0,0 +1,26 @@
+package A01;
+
+
+public class Percolation {
+	
+	public Percolation(int N) {
+		
+	}
+	
+	public void open(int i, int j) {
+		
+	}
+	
+	public boolean isOpen(int i, int j) {
+		return false;
+	}
+	
+	public boolean isFull(int i, int j) {
+		return false;
+	}
+	
+	public boolean percolates() {
+		return false;
+	}
+
+}
---
+++ A01 Percolation/src/A01/PercolationStats.java
@@ -0,0 +1,24 @@
+package A01;
+
+public class PercolationStats {
+
+	public PercolationStats(int N, int T) {
+		
+	}
+	
+	public double mean() {
+		return 0;
+	}
+	
+	public double stddev() {
+		return 0;	
+	}
+	
+	public double confidenceLow() {
+		return 0;
+	}
+	
+	public double confidenceHigh() {
+		return 0;
+	}
+}
