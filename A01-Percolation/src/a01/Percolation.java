c3792a68f4f4d631a99f1a76c159424e51ef0965
kelsieeringarcia <36800046+kelsieeringarcia@users.noreply.github.com>
2021-01-29 20:21:17 -0700

    finished percolation part 1

--- A01-Percolation/src/a01/Percolation.java
+++ A01-Percolation/src/a01/Percolation.java
@@ -4,7 +4,7 @@ import a01.Percolation;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
- * @author chad.zuniga
+ * @author Chad Zuniga and Kelsie Garcia
  *
  */
 public class Percolation {
@@ -17,33 +17,41 @@ public class Percolation {
 	private int count;
 	
 	/**
+	 * This constructs the grid for percolation and unions the top row to the virtual top
+	 * and the bottom row with the virtual bottom
 	 * 
-	 * @param n
+	 * @param n size of the grid
 	 */
 	public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Size of grid must be greater than 0.");
+        }
 		size = n;
 		grid = new boolean[n][n];
 		count = 0;
 		
-		fullArray = new WeightedQuickUnionUF(n*n+2);
-		bwArray = new WeightedQuickUnionUF(n*n+2);
-		vTop = n*n;
-		vBot = n*n + 1;
+		fullArray = new WeightedQuickUnionUF(n * n + 2);
+		bwArray = new WeightedQuickUnionUF(n * n + 2);
+		vTop = n * n;
+		vBot = n * n + 1;
 		
+		//connects the virtual top to the top row
 		for(int i = 0; i < size; i++) {
 			fullArray.union(vTop, i);
 			bwArray.union(vTop, i);
 		}
 		
-		//only unions virtual bottom with Backwash QU array
-		int lastRowStart = size*size - size;
-		for(int i = lastRowStart; i < size*size; i++) {
+		//only unions virtual bottom with Back wash QU array
+		int lastRowStart = size * size - size;
+		for(int i = lastRowStart; i < size * size; i++) {
 			bwArray.union(vBot, i);
 		}
 		
 	}
 	
 	/**
+	 * This method checks if the spot is opened and if the surrounding spots are
+	 * open so they can be put in a union
 	 * 
 	 * @param i  Y axis value
 	 * @param j  X axis value
@@ -55,33 +63,35 @@ public class Percolation {
 		}
 		
 		//above
-		if(i > 0 && isOpen(i-1,j)) {
-			fullArray.union( xyTo1d(i,j), xyTo1d(i-1,j) );
-			bwArray.union( xyTo1d(i,j), xyTo1d(i-1,j) );
+		if(i > 0 && isOpen(i - 1,j)) {
+			fullArray.union( xyTo1d(i,j), xyTo1d(i - 1,j) );
+			bwArray.union( xyTo1d(i,j), xyTo1d(i - 1,j) );
 		}
 		
 		//right
-		if(j < size-1 && isOpen(i,j+1)) {
-			fullArray.union( xyTo1d(i,j), xyTo1d(i,j+1) );
-			bwArray.union( xyTo1d(i,j), xyTo1d(i,j+1) );
+		if(j < size - 1 && isOpen(i,j + 1)) {
+			fullArray.union( xyTo1d(i,j), xyTo1d(i,j + 1) );
+			bwArray.union( xyTo1d(i,j), xyTo1d(i,j + 1) );
 
 		}
 		
 		//bottom
-		if(i < size-1 && isOpen(i+1,j)) {
-			fullArray.union( xyTo1d(i,j), xyTo1d(i+1,j) );
-			bwArray.union( xyTo1d(i,j), xyTo1d(i+1,j) );
+		if(i < size - 1 && isOpen(i + 1,j)) {
+			fullArray.union( xyTo1d(i,j), xyTo1d(i + 1,j) );
+			bwArray.union( xyTo1d(i,j), xyTo1d(i + 1,j) );
 
 		}
 		
 		//left
-		if(j > 0 && isOpen(i,j-1)) {
-			fullArray.union( xyTo1d(i,j), xyTo1d(i,j-1) );
-			bwArray.union( xyTo1d(i,j), xyTo1d(i,j-1) );
+		if(j > 0 && isOpen(i,j - 1)) {
+			fullArray.union( xyTo1d(i,j), xyTo1d(i,j - 1) );
+			bwArray.union( xyTo1d(i,j), xyTo1d(i,j - 1) );
 		}
 	}
 	
 	/**
+	 * This method returns the amount of spots opened in the grid
+	 * 
 	 * @return the count
 	 */
 	public int getCount() {
@@ -89,14 +99,17 @@ public class Percolation {
 	}
 
 	/**
+	 * This method checks to see if the spot is open in the grid.
 	 * 
-	 * @return
+	 * @return if the spot is open
 	 */
 	public boolean isOpen(int i, int j) {
 		return this.grid[i][j];
 	}
 	
 	/**
+	 * This method returns if the opened spots are connected with the top and are full
+	 * and this method does not reach the bottom to avoid back wash.
 	 * 
 	 * @param i
 	 * @param j
@@ -107,6 +120,7 @@ public class Percolation {
 	}
 	
 	/**
+	 * This method checks if the virtual top is connected with the virtual bottom
 	 * 
 	 * @return
 	 */
--- A01-Percolation/src/a01/PercolationStats.java
+++ A01-Percolation/src/a01/PercolationStats.java
@@ -2,17 +2,24 @@ package a01;
 
 import java.util.Random;
 
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 /**
- * @author chad.zuniga
+ * @author Chad Zuniga and Kelsie Garcia
  *
  */
 public class PercolationStats {
 	Random rand = new Random();
 	private double[] stats;
 
+	/**
+	 * This constructor simulates percolation at a given amount when the spots are opened
+	 * at random. The results are stored in a array to be used to give statistics on the 
+	 * simulation.
+	 *  
+	 * @param N size of the grid
+	 * @param T number of trials in the simulation
+	 */
 	public PercolationStats(int N, int T) {
         if (N <= 0) {
             throw new IllegalArgumentException("Size of grid must be greater than 0.");
@@ -21,27 +28,51 @@ public class PercolationStats {
             throw new IllegalArgumentException("There must be more than 0 trails.");
         }
 		stats = new double[T];
+		//iterates through all the trials to add the results of each percolation to the stats array
 		for(int i = 0; i < T; i++) {
 			Percolation p = new Percolation(N);
 			while(!p.percolates()) {
 				p.open(rand.nextInt(N),rand.nextInt(N));
 			}
+			//number of slots opened * double(changing int to double) / size^2
 			stats[i] = p.getCount() * 1.0 / (N * N);
 		}
     }
 
+	/**
+	 * This method will take the stats from the simulated percolation and get the mean 
+	 * 
+	 * @return
+	 */
     public double mean() {
         return StdStats.mean(stats);
     }
 
+    /**
+     * This method will take the stats from the simulated percolation and get the standard deviation
+     * 
+     * @return
+     */
     public double stddev() {
         return StdStats.stddev(stats);
     }
 
+    /**
+     * This method will take the stats from the simulated percolation and get the confidence low
+     * at 95% end point
+     * 
+     * @return
+     */
     public double confidenceLow() {
         return mean() - 1.96 * stddev() / Math.sqrt(stats.length);
     }
 
+    /**
+     * This method will take the stats from the simulated percolation and get the confidence high
+     * at 95% end point
+     * 
+     * @return
+     */
     public double confidenceHigh() {
         return mean() + 1.96 * stddev() / Math.sqrt(stats.length);
     }
